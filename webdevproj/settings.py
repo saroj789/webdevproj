a60e851e80af77a1079cb2071f43998a87970ab9
saroj789 <sarojkumarvaishya@gmail.com>
2021-03-29 19:05:11 +0530

    'deploy3'

--- webdevproj/settings.py
+++ webdevproj/settings.py
@@ -15,10 +15,11 @@ import os
 
 import django_heroku
 django_heroku.settings(locals())
-DISABLE_COLLECTSTATIC=1
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+#BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -130,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 STATIC_DIR= os.path.join(BASE_DIR,STATIC_URL)
 STATICFILES_DIR=[STATIC_DIR]
